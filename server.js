a12f6e04bdc8ff9f899b13055b22ab0445467a87
kavithamernstack <kavitt.29@gmail.com>
2025-09-20 22:15:31 +0530

    det

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.use("/uploads", express.static("uploads", {
 app.get('/', (req, res) => res.json({ status: 'ok' }))
 
 // API routes
-app.use('/api/auth', authRoutes)
+app.use('/auth', authRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/posts', postRoutes)
 app.use('/api/comments', commentRoutes)
