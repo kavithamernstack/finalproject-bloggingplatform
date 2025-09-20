2cc6c02576a12af178b859b82eccdd84b09e9548
kavithamernstack <kavitt.29@gmail.com>
2025-09-20 16:24:35 +0530

    new modify

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const app = express()
 
 // Middlewares
 app.use(cors({
-  origin: process.env.CLIENT_URL,       // http://localhost:3000
+  origin: process.env.CLIENT_URL,       
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
